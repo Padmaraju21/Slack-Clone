2007608173a8c0d5cf66e977009266a92b192132
pencil <penciljamnoreply@gmail.com>
2021-03-08 18:54:47 +0000

    db changes

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import "firebase/storage";
 var config = {
   apiKey: "AIzaSyAYFbGya8DFxXxn1PBH6_9c28QL0nEsQDQ",
   authDomain: "slack-clone-ef5ef.firebaseapp.com",
+  databaseURL: "https://slack-clone-ef5ef-default-rtdb.europe-west1.firebasedatabase.app/"
   projectId: "slack-clone-ef5ef",
   storageBucket: "slack-clone-ef5ef.appspot.com",
   messagingSenderId: "780298464513",
